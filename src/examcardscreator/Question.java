fc0ee08e604df773f5c5ae7e74d8a1721ad35fc3
1 <1@1-VAIO>
2017-11-23 18:59:02 +0300

    Изменена логика обмена вопросами между билетами

--- src/examcardscreator/Question.java
+++ src/examcardscreator/Question.java
@@ -16,9 +16,10 @@ public class Question
     public Question(String str)
     {
         String[] elems = str.split(";");
-        this.theme = elems[0];
-        this.text = elems[1];
-        this.complexity = Integer.parseInt(elems[2]);
+        int tmp = Integer.parseInt(elems[0].trim());
+        this.theme = elems[0].trim();
+        this.text = elems[1].trim();
+        this.complexity = Integer.parseInt(elems[2].trim());
     }
     
     @Override
